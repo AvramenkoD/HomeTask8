18ece9aeb9b4bb2780ba8c51b140dbbcef8f4d12
AvramenkoD <dsdautor227@gmail.com>
2025-10-18 14:51:33 +0300

    Fix N

--- source.c
+++ source.c
@@ -1,25 +1,21 @@
 ﻿#include <stdio.h>
-
+#include <stdlib.h>
 int main() {
 	
 	unsigned int n;
 	double rez = 1;
+	char string[32];
 
 	puts("Enter N:");
 	scanf("%u", &n);
 
-	if (n >= 10)
-	{ 
-		puts("Error"); 
-		return -1;
-	}
-
 	for (unsigned int i = 1; i <= n; i++)
 	{
-		rez *= 1. + i*0.1;
+		sprintf(string, "1.%u", i);
+		rez *= atof(string);
 	}
 
 	printf("Result = %f", rez);
 
-	system("pause");
+	return 0;
 }
