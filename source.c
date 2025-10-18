26b7b5ef4e07e1c13b6a688141437a801f26c07c
AvramenkoD <dsdautor227@gmail.com>
2025-10-18 14:30:18 +0300

    Добавьте файлы проекта.

---
+++ source.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main() {
+	
+	unsigned int n;
+	double rez = 1;
+
+	puts("Enter N:");
+	scanf("%u", &n);
+
+	if (n >= 10)
+	{ 
+		puts("Error"); 
+		return -1;
+	}
+
+	for (unsigned int i = 1; i <= n; i++)
+	{
+		rez *= 1. + i*0.1;
+	}
+
+	printf("Result = %f", rez);
+
+	system("pause");
+}
